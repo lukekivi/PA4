14e9c1d245e82e986d194aef44dceb51f96152b4
Viet Nguyen <nguy4471@umn.edu>
2021-12-15 20:25:04 -0600

    Header, test on cse lab machines

--- src/server.c
+++ src/server.c
@@ -1,3 +1,8 @@
+/*test machine: csel-potato.cselabs.umn.edu
+ * group number: G[45]
+ * name: Lucas Kivi, Dallas Schauer, Viet Nguyen
+ * x500: kivix019, schau364, nguy4471 */
+
 #include "server.h"
 #define STARTING_TRANSACTIONS_SIZE 50
 
